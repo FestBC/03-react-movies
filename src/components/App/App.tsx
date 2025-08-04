e5b1f9e881b207582015204b3657eaec1c05684d
Fest <brychkapovozka@gmail.com>
2025-08-04 05:46:30 +0200

    Add some components.

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,12 +1,41 @@
+import { useState } from "react";
+
 import css from "./App.module.css";
 
+import toast, { Toaster } from "react-hot-toast";
+
+import { type Movie } from "../../types/movie";
 import { fetchTmdb } from "../../services/movieService";
 
+import SearchBar from "../SearchBar/SearchBar";
+import MovieGrid from "../MovieGrid/MovieGrid";
+
 export default function App() {
-  
+  const [movies, setMovies] = useState<Movie[]>([]);
+
+  const handleSubmit = async (query: string) => {
+    try {
+      const results = await fetchTmdb(query);
+
+      if (results.length > 0) {
+        setMovies(results);
+      } else {
+        toast.error("No movies found for your request.");
+      }
+    } catch {
+      toast.error("Something went wrong...");
+    }
+  }
+
+  const handleSelect = (event: React.MouseEvent<HTMLLIElement>): void => {
+    console.log(event.target);
+  }
+
   return (
-    <>
-      
-    </>
+    <div className={css.app}>
+      <Toaster />
+      <SearchBar onSubmit={handleSubmit} />
+      {movies.length > 0 && <MovieGrid onSelect={handleSelect} movies={movies} />}
+    </div>
   );
 }
---
+++ src/components/MovieGrid/MovieGrid.tsx
@@ -0,0 +1,27 @@
+import css from "./MovieGrid.module.css";
+
+import { type Movie } from "../../types/movie";
+
+interface MovieGridProps {
+    onSelect: (event: React.MouseEvent<HTMLLIElement>) => void,
+    movies: Movie[]
+}
+
+export default function MovieGrid({ onSelect, movies }: MovieGridProps) {
+    return (
+        <ul className={css.grid}>
+            {movies.map(({ id, poster_path, title }) => (
+                <li key={id} onClick={onSelect}>
+                    <div className={css.card}>
+                        <img className={css.image}
+                             src={`https://image.tmdb.org/t/p/w500${poster_path}`}
+                             alt={title}
+                             loading="lazy"
+                        />
+                        <h2 className={css.title}>{title}</h2>
+                    </div>
+                </li>
+            ))}
+        </ul>
+    );
+}
